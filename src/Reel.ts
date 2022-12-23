3b9bd5755e33540924841a78ed5911da34a13d5f
VlazaYuh <vlad.death11@gmail.com>
2022-12-23 23:51:42 +0300

    fixed mask+ masked sprite

--- src/Reel.ts
+++ src/Reel.ts
@@ -3,18 +3,19 @@ import { icons } from './icons'
 export class Reel extends PIXI.Container {
     private container = this.addChild(new PIXI.Container())
     //
-    private symbolHeight = 50
+    private symbolSize = 50
     constructor(private readonly rows: number) {
         super()
-        this.container.pivot.y = this.symbolHeight * (rows - 1) / 2 /* this.container */
-        for (let i = rows; i--;) {
+        this.addChild(new PIXI.Graphics().beginFill(0xffffff).drawCircle(0,0,5))
+        this.container.y = -this.symbolSize * (rows - 1) / 2
+        this.createMask()
+        for (let i = rows+1; i--;) {
             const sprite = this.container.addChild(this.getSprite())
-            sprite.y = i * this.symbolHeight
-            this.createMask()
+            sprite.y = (i-1) * this.symbolSize
         }
     }
     start() {
-        
+
     }
     stop() {
 
@@ -26,16 +27,9 @@ export class Reel extends PIXI.Container {
     }
     private createMask(offset = 15) {
         const mask = new PIXI.Graphics()
-        mask.lineStyle(0)
-        mask.pivot.x = this.container.pivot.x + offset
-        mask.pivot.y = this.container.pivot.y + offset + this.symbolHeight
-        mask.position.set(this.container.x, this.container.y)
-        mask.beginFill(0x660000, 0.5)
-        /*       mask.drawRect(mask.x, mask.y, this.container.width, this.symbolHeight * (this.rows + 1.5)) */
-        mask.drawRect(mask.x, mask.y, this.container.width, this.symbolHeight)
-        mask.drawRect(mask.x, this.symbolHeight * (this.rows + 0.6), this.container.width, this.symbolHeight)
-        mask.endFill()
-       /*  this.mask = mask */
+        mask.beginFill(0,0)
+        mask.drawRect(-this.symbolSize / 2, -this.symbolSize * this.rows / 2, this.symbolSize, this.symbolSize * this.rows)
+        this.container.mask = mask
         this.addChild(mask)
 
     }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Reel } from './Reel'
 const app = new PIXI.Application({ sharedTicker: true, sharedLoader: true, /* backgroundColor: 1099 */})
 document.body.appendChild(app.view)
 function init() {
-    const reel = window.reel = app.stage.addChild(new Reel(3))
+    const reel = window.reel = app.stage.addChild(new Reel(5))
     reel.position.set(app.screen.width / 2, app.screen.height / 2)
 }
 PIXI.Loader.shared.add('assets/fruits.json')
