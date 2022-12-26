d25d125d058fd4839dbd333fcac97a4ab9b4e59d
VlazaYuh <vlad.death11@gmail.com>
2022-12-26 22:38:33 +0300

    staet and stop added

--- src/Reel.ts
+++ src/Reel.ts
@@ -1,30 +1,43 @@
 import * as PIXI from 'pixi.js'
 import { icons } from './icons'
 import { Ticker } from 'pixi.js'
+import { app } from '.'
 export class Reel extends PIXI.Container {
     private container = this.addChild(new PIXI.Container())
     //
     private symbolSize = 50
     constructor(private readonly rows: number) {
         super()
-  /*       this.addChild(new PIXI.Graphics().beginFill(0xffffff).drawCircle(0,0,5)) */
+        /* this.addChild(new PIXI.Graphics().beginFill(0xffffff).drawCircle(0,0,5))  */
+
         this.container.y = -this.symbolSize * (rows - 1) / 2
         this.createMask()
-        for (let i = rows+1; i--;) {
-            const sprite = this.container.addChild(this.getSprite())
-            sprite.y = (i-1) * this.symbolSize
-        }
+        this.createSymbols(rows + 1)
     }
-    start() {
+    start(speed = 1) {
         const ticker = PIXI.Ticker.shared
-        ticker.add(() =>{
-            this.container.y-0.1
+        ticker.autoStart = false
+        let yOffset = (app.screen.height - this.container.children[0].y - this.symbolSize /* * this.rows */)
+        ticker.add(() => {
+            for (let i = this.container.children.length - 1; i >= 0; i--) {
+                const symbol = this.container.children[i]
+
+                if (this.container.children[0].y + speed > app.screen.height - yOffset) {
+                    this.container.children[0].destroy()
+                    this.createSymbols()
+                    /* ticker.stop() */
+                }
+                else {
+                    symbol.y += speed
+                }
+            }
+
         })
-        ticker.autoStart=false
         ticker.start()
     }
+ 
     stop() {
-
+        PIXI.Ticker.shared.stop()
     }
     private getSprite(id = Math.floor(Math.random() * icons.length)) {
         const sprite = PIXI.Sprite.from(icons[id])
@@ -33,10 +46,16 @@ export class Reel extends PIXI.Container {
     }
     private createMask(offset = 15) {
         const mask = new PIXI.Graphics()
-        mask.beginFill(0,0)
+        mask.beginFill(0, 0)
         mask.drawRect(-this.symbolSize / 2, -this.symbolSize * this.rows / 2, this.symbolSize, this.symbolSize * this.rows)
         this.container.mask = mask
         this.addChild(mask)
 
+    }
+    private createSymbols(amount = 1) {
+        for (let i = amount; i--;) {
+            const sprite = this.container.addChild(this.getSprite())
+            sprite.y = (i - 1) * this.symbolSize
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import * as PIXI from 'pixi.js'
 import { Reel } from './Reel'
-const app = new PIXI.Application({ sharedTicker: true, sharedLoader: true, /* backgroundColor: 1099 */})
+export const app = new PIXI.Application({ sharedTicker: true, sharedLoader: true, /* backgroundColor: 1099 */ })
 document.body.appendChild(app.view)
 function init() {
     const reel = window.reel = app.stage.addChild(new Reel(5))
     reel.position.set(app.screen.width / 2, app.screen.height / 2)
-  /*   reel.start() */
+    const button = new PIXI.Graphics()
+    button.beginFill(0x660000, 1)
+    button.drawRect(app.screen.width/2+ 100, 50*5*2, 50, 20)
+    button.interactive = true
+    button.cursor = 'pointer'
+    button.addListener('pointerdown', () => {
+        reel.start()
+    })
+    app.stage.addChild(button)
+    const button2 = new PIXI.Graphics()
+    button2.beginFill(0x660000, 1)
+    button2.drawRect(app.screen.width/2 - 100, 50*5*2, 50, 20)
+    button2.interactive = true
+    button2.cursor = 'pointer'
+    button2.addListener('pointerdown', () => {
+        reel.stop()
+    })
+    app.stage.addChild(button2)
 }
 PIXI.Loader.shared.add('assets/fruits.json')
 PIXI.Loader.shared.load(init)
