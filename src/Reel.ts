c1324d10ce540b41e9e571b38731da854dfe2bd6
VlazaYuh <vlad.death11@gmail.com>
2023-01-05 18:19:23 +0300

    logic of ticker replaced+stop bugfix

--- src/Reel.ts
+++ src/Reel.ts
@@ -4,52 +4,64 @@ import { Ticker } from 'pixi.js'
 import { app } from '.'
 export class Reel extends PIXI.Container {
     private container = this.addChild(new PIXI.Container())
-    private speed = 2
+    private speed = 0.5
     private symbolSize = 50
     private running = false
-    private yoffset: number
+    private stopping: boolean
+    private yOffset: number
+    private i: number
     private funct = (delta: number) => {
 
-        for (let i = this.container.children.length - 1; i >= 0; i--) {
-            const symbol = this.container.children[i]
-            if (this.container.children[0].y + delta * this.speed > this.yoffset) {
-                this.container.children[0].destroy()
-                this.createSymbols()
+        if (this.container.y + delta * this.speed > this.i + this.symbolSize) {
+            /* this.i += this.symbolSize */
+            this.container.y -= this.symbolSize
+            this.container.children.forEach((child) => child.y += this.symbolSize)
+            this.container.children[0].destroy()
+            this.createSymbols()
+            if (this.stopping && Math.floor(this.container.children[0].y) === this.yOffset - this.symbolSize) {
+                this.running = false
+                this.stopping = false
+                PIXI.Ticker.shared.remove(this.funct)
             }
-            else {
-                symbol.y += delta * this.speed
-            }
+
         }
+        else {
+/*             if (this.stopping && (this.container.y) >= this.yOffset - this.symbolSize) {
+                this.running = false
+                this.stopping = false
+                PIXI.Ticker.shared.remove(this.funct)
+            } */
+            this.container.y += delta * this.speed
+
+        }
+
 
     }
     constructor(private readonly rows: number) {
         super()
         /* this.addChild(new PIXI.Graphics().beginFill(0xffffff).drawCircle(0,0,5))  */
         this.container.y = -this.symbolSize * (rows - 1) / 2
+        this.i = this.container.y
         this.createMask()
         this.createSymbols(rows + 1)
-        this.yoffset = (this.container.children[0].y + this.symbolSize)
+        this.yOffset = (this.symbolSize * rows)
         /* const yOffset = (app.screen.height - this.container.children[0].y - this.symbolSize) */
     }
     start() {
         if (this.running) {
+            return
         }
-        else {
-            const ticker = PIXI.Ticker.shared
-
-            ticker.add(this.funct)
-
-
-            ticker.start()
-            this.running = true
-        }
+        PIXI.Ticker.shared.add(this.funct)
+        this.running = true
+        this.stopping = false
     }
 
-    async stop() {
+    async stop(child: object) {
         /* if (this.container.children[0].y == this.yoffset - this.symbolSize) { */
-            PIXI.Ticker.shared.stop()
-            this.running = false
-            PIXI.Ticker.shared.remove(this.funct)
+        /*  PIXI.Ticker.shared.stop() */
+        this.stopping = true
+        /* this.running = false
+        PIXI.Ticker.shared.remove(this.funct) */
 
     }
     private getSprite(id = Math.floor(Math.random() * icons.length)) {
@@ -71,5 +83,4 @@ export class Reel extends PIXI.Container {
             sprite.y = (amount - 1) * this.symbolSize
         }
     }
-
 }
