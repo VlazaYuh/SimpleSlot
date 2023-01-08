24feb1bf78c8c349dcd1f976091e74d2e9774346
VlazaYuh <vlad.death11@gmail.com>
2023-01-08 18:35:21 +0300

    format

--- src/Reel.ts
+++ src/Reel.ts
@@ -10,10 +10,9 @@ export class Reel extends PIXI.Container {
     private containerOffset: number
     private queue: Array<number> = []
     private moveContainer = (delta: number) => {
-
         if (this.container.y + delta * this.speed >= this.containerOffset + this.symbolSize) {
             let symbol: number
-            this.container.y -= this.symbolSize-delta*this.speed 
+            this.container.y -= this.symbolSize - delta * this.speed
             this.container.children.forEach((child) => child.y += this.symbolSize /* + delta * this.speed */)
             this.container.children[0].destroy()
             if (this.queue) {
@@ -21,7 +20,6 @@ export class Reel extends PIXI.Container {
                 this.createSymbols(1, symbol)
             }
             else {
-
                 this.createSymbols()
             }
             if (this.stopping && this.container.children[0].y >= this.yOffset - this.symbolSize && symbol == undefined && this.queue.length === 0) {
@@ -32,7 +30,6 @@ export class Reel extends PIXI.Container {
         }
         else {
             this.container.y += delta * this.speed
-
         }
     }
     constructor(private readonly rows: number) {
@@ -52,21 +49,18 @@ export class Reel extends PIXI.Container {
         this.running = true
         this.stopping = false
     }
-
     async stop(id?: Array<number>) {
         if (id) {
-            if(id.length===icons.length){
-            if(id.every(elem=>elem>=0&&elem<icons.length)){
-                this.queue = id
+            if (id.length === icons.length) {
+                if (id.every(elem => elem >= 0 && elem < icons.length)) {
+                    this.queue = id
+                }
+                else {
+                    throw 'Non valid array'
+                }
             }
-            else{
-                throw 'Non valid array'
-            }
-        }
         }
         this.stopping = true
-
-
     }
     private getSprite(id = Math.floor(Math.random() * icons.length)) {
         const sprite = PIXI.Sprite.from(icons[id])
@@ -77,14 +71,10 @@ export class Reel extends PIXI.Container {
         const mask = new PIXI.Graphics()
         mask.beginFill(0, 0)
         mask.drawRect(-this.symbolSize / 2, -this.symbolSize * this.rows / 2, this.symbolSize, this.symbolSize * this.rows)
-        this.container.mask = mask 
+        this.container.mask = mask
         this.addChild(mask)
-
     }
     private createSymbols(amount = 1, id?: number) {
-        /*         if (id = undefined) {
-                    id = Math.floor(Math.random() * icons.length)
-                } */
         while (amount--) {
             const sprite = this.container.addChild(this.getSprite(id))
             sprite.y = (amount - 1) * this.symbolSize
