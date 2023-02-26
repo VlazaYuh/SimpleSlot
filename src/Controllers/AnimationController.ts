a40003cda7a00d585ebc1b0196d2c5e6a9d59c48
VlazaYuh <vlad.death11@gmail.com>
2023-02-26 23:24:45 +0300

    formatting

--- src/Controllers/AnimationController.ts
+++ src/Controllers/AnimationController.ts
@@ -12,17 +12,12 @@ window.gsap = gsap
 export class AnimationController extends Controller {
     private emmiters: Array<Emitter> = []
     private reels: Reels
-    private particleContainer:PIXI.ParticleContainer 
-    private linesAnim:Lines
-    constructor(reels: Reels,lineAnim:Lines) {
+    private particleContainer: PIXI.ParticleContainer
+    private linesAnim: Lines
+    constructor(reels: Reels, lineAnim: Lines) {
         super()
         this.reels = reels
-        this.linesAnim=lineAnim
-        this.particleContainer=new PIXI.ParticleContainer(this.reels.rows*500)
-        app.stage.addChild(this.particleContainer)
-        for (let i = 0; i < reels.rows; i++) {
-            this.emmiters.push(new Emitter(this.particleContainer, upgradeConfig(PIXI.Loader.shared.resources['assets/particles.json'].data, PIXI.Texture.from('assets/Snow50px.png'))))
-        }
+        this.linesAnim = lineAnim
     }
     protected async stateChangeCallback(state: State): Promise<void> {
         if (state === State.Animation) {
@@ -39,22 +34,5 @@ export class AnimationController extends Controller {
     }
     private isWinSymbol(row: number, column: number) {
         return getSpinResult().win.lines.some(line => getLinesDict()[line.id][column] === row && line.reels.includes(column))
-    }
-    private async playLines1(line: number[]) {
-        const promiseArray: Promise<void>[] = []
-        for (let i1 = 0; i1 < line.length; i1++) {
-            this.emmiters[i1].autoUpdate = true
-            this.emmiters[i1].updateSpawnPos(this.reels.getSymbol(0, getLinesDict()[line[i1]][0]).toGlobal(app.stage).x,
-                this.reels.getSymbol(0, getLinesDict()[line[i1]][0]).toGlobal(app.stage).y)
-            for (let i = 0; i < getLinesDict()[line[i1]].length; i++) {
-                this.reels.getSymbol(i, getLinesDict()[line[i1]][i]).toGlobal(app.stage) //имплементировать движение
-            }
-        }
-        await Promise.all(promiseArray)
-        await delay(500)
-        this.emmiters.forEach(emmiter=>{
-            emmiter.autoUpdate=false
-            emmiter.cleanup()
-        })
     }
 }
--- src/lines.ts
+++ src/lines.ts
@@ -5,16 +5,19 @@ import { getLinesDict } from "./linesDict"
 import { getSpinResult } from "./spinResult"
 import { app } from '.'
 import { Emitter, upgradeConfig } from '@pixi/particle-emitter'
-import { emitterXY } from './emitterXY'
 export class Lines extends PIXI.Container {
     private particleContainer: PIXI.ParticleContainer
     private emmiters: Array<Emitter> = []
     private reels: Reels
+    private emitterXY = {
+        x: 0,
+        y: 0
+    }
     constructor(reels: Reels) {
         super()
         this.reels = reels
         this.particleContainer = this.addChild(new PIXI.ParticleContainer(this.reels.rows * 500))
-        for (let i = 0; i < reels.rows; i++) {
+        for (let i = 0; i < getLinesDict().length; i++) {
             this.emmiters.push(new Emitter(this.particleContainer, upgradeConfig(PIXI.Loader.shared.resources['assets/particles.json'].data, PIXI.Texture.from('assets/Snow50px.png'))))
         }
     }
@@ -23,6 +26,35 @@ export class Lines extends PIXI.Container {
         let coordinates: Array<PIXI.IPointData[]> = []
         let pifagorSum: number[] = []
         let ratios: Array<number[]> = []
+        this.calculations(pifagor,coordinates)
+        this.ratiosCalculations(pifagor,pifagorSum,ratios)
+        this.animation(coordinates,ratios)
+    }
+    animation(coordinates: Array<PIXI.IPointData[]>,ratios: Array<number[]>){
+        for (let i = 0; i < getSpinResult().win.lines.length; i++) {
+            let timeline = gsap.timeline({
+                onStart: () => { this.emitterXY.x = this.emmiters[i].spawnPos.x, this.emitterXY.y = this.emmiters[i].spawnPos.y, this.emmiters[i].autoUpdate = true },
+                onUpdate: () => { this.emmiters[i].updateSpawnPos(this.emitterXY.x, this.emitterXY.y) },
+                onComplete: () => { this.emmiters[i].autoUpdate = false, this.emmiters[i].cleanup() }
+            })
+            for (let i1 = 0; i1 < this.reels.rows - 1; i1++) {
+                timeline.to(this.emitterXY, { x: coordinates[i][i1].x, y: coordinates[i][i1].y, duration: 0.5 * (this.reels.rows - 1) * ratios[i][i1] })
+            }
+        }
+    }
+    ratiosCalculations(pifagor: Array<number[]>,pifagorSum: number[],ratios: Array<number[]>){
+        for (let i = 0; i < pifagor.length; i++) {
+            pifagorSum.push(...[pifagor[i].reduce((a, b) => a + b, 0)])
+            let tempArray: number[] = []
+            for (let i2 = 0; i2 < this.reels.rows - 1; i2++) {
+                tempArray.push(pifagor[i][i2] / pifagorSum[i])
+                if (tempArray.length === this.reels.rows - 1) {
+                    ratios.push(...[tempArray])
+                }
+            }
+        }
+    }
+    calculations(pifagor: Array<number[]>,coordinates: Array<PIXI.IPointData[]>){
         for (let i1 = 0; i1 < getSpinResult().win.lines.length; i1++) {
             this.emmiters[i1].updateSpawnPos(this.reels.getSymbol(0, getLinesDict()[getSpinResult().win.lines[i1].id][0]).toGlobal(app.stage).x,
                 this.reels.getSymbol(0, getLinesDict()[getSpinResult().win.lines[i1].id][0]).toGlobal(app.stage).y)
@@ -39,30 +71,6 @@ export class Lines extends PIXI.Container {
                         tempArray.shift()
                     }
                 }
-            }
-        }
-        for (let i = 0; i < pifagor.length; i++) {
-            pifagorSum.push(...[pifagor[i].reduce((a, b) => a + b, 0)])
-            let tempArray: number[] = []
-            for (let i2 = 0; i2 < this.reels.rows - 1; i2++) {
-                tempArray.push(pifagor[i][i2] / pifagorSum[i])
-                if (tempArray.length === this.reels.rows - 1) {
-                    ratios.push(...[tempArray])
-                }
-            }
-        }
-        console.log(pifagor)
-        console.log(pifagorSum)
-        console.log(ratios)
-        console.log(coordinates)
-        for (let i = 0; i < getSpinResult().win.lines.length; i++) {
-            let timeline = gsap.timeline({
-                onStart: () => { emitterXY.x = this.emmiters[i].spawnPos.x, emitterXY.y = this.emmiters[i].spawnPos.y, this.emmiters[i].autoUpdate = true },
-                onUpdate: () => { this.emmiters[i].updateSpawnPos(emitterXY.x, emitterXY.y) },
-                onComplete: () => { this.emmiters[i].autoUpdate = false, this.emmiters[i].cleanup() }
-            })
-            for (let i1 = 0; i1 < this.reels.rows - 1; i1++) {
-                timeline.to(emitterXY, { x: coordinates[i][i1].x, y: coordinates[i][i1].y, duration: 0.5 * (this.reels.rows - 1) * ratios[i][i1] })
             }
         }
     }
